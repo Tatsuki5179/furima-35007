409aa2ffa2ab5c34a970891d21838d69fe342fc2
山下龍希 <tatsuki1105179@icloud.com>
2021-04-23 15:33:16 +0900

    ユーザー管理機能修正6

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,16 @@ class User < ApplicationRecord
           validates :nickname
           validates :password,             format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze}
           validates :birth_date
-          validates :last_name,              format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-          validates :first_name,             format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-          validates :last_name_kana,          format: {with: /\A[ァ-ヶー－]+\z/ }
-          validates :first_name_kana,         format: {with: /\A[ァ-ヶー－]+\z/ }
          end
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+          validates :last_name
+          validates :first_name
+        end
+      
+        
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }do
+          validates :last_name_kana
+          validates :first_name_kana
+        end
 end
