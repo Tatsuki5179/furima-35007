1a599bca25837a8be1576d4cb810b8436f7b654f
山下龍希 <tatsuki1105179@icloud.com>
2021-04-26 18:17:00 +0900

    validationの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :days_ship
+  belongs_to :product_condition
+  belongs_to :shipping_area
+  belongs_to :shipping_charge
+
   with_options presence: true do
     validates :product_name
     validates :product_description
     validates :product_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    validates :product_condition_id
-    validates :shipping_charge_id
-    validates :shipping_area_id
-    validates :days_ship_id
-    validates :category_id
+    validates :product_condition_id,  numericality: { other_than: 0, message: "Select" }
+    validates :shipping_charge_id, numericality: { other_than: 0, message: "Select" }
+    validates :shipping_area_id,  numericality: { other_than: 0, message: "Select" }
+    validates :days_ship_id,  numericality: { other_than: 0, message: "Select" }
+    validates :category_id,  numericality: { other_than: 0, message: "Select" }
     validates :image
-
   end
 end
