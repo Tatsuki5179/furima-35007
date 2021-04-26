0ff43c5090c941c9abb4b23164e27f729b82140e
山下龍希 <tatsuki1105179@icloud.com>
2021-04-26 15:04:57 +0900

    商品出品ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,36 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, only: :new
+
   def index
+    @item = Item.all
   end
 
   def new
+    @item = Item.new
   end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end    
+  end
+
+    
+  
+
+
+
+
 
 
 
   private
 
   def item_params
-    params.require(:item).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :product_description, :product_price, :product_condition_id, :shipping_charge_id, :shipping_area_id, :days_ship_id, :category_id, :user).merge(user_id: current_user.id)
   end
 
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :product_name
+    validates :product_description
+    validates :product_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :product_condition_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :days_ship_id
+    validates :category_id
+    validates :image
+
+  end
 end
