3222f5cb664ef9dc8e8ca312a582d98359fb3df9
山下龍希 <tatsuki1105179@icloud.com>
2021-04-26 20:09:58 +0900

    商品出品機能修正1

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
+
+
   factory :item do
-    
+    product_name             {"hoge"}
+    product_description      {Faker::Lorem.sentence}
+    product_condition_id     {1}
+    shipping_charge_id       {1}
+    shipping_area_id         {1}
+    days_ship_id             {1}
+    product_price            {"9999"}
+    user_id                  {1}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品情報入力' do
+    
+    context '商品情報の入力がうまく行かない時' do
+      it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空だと出品できない' do
+        @item.product_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product name can't be blank")
+      end
+      it 'product_descriptionが空だと出品できない' do
+        @item.product_description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product description can't be blank")
+      end
+      it 'categoryが未選択だと出品できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'product_conditionが未選択だと出品できない' do
+        @item.product_condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product condition can't be blank")
+      end
+      it 'shipping_chargeが未選択だと出品できない' do
+        @item.shipping_charge_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
+      end
+      it 'shipping_areaが未選択だと出品できない' do
+        @item.shipping_area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area can't be blank")
+      end
+      it 'days_shipが未選択だと出品できない' do
+        @item.days_ship_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days ship can't be blank")
+      end
+      it 'product_priceが空だと出品できない' do
+        @item.product_price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product price can't be blank")
+      end
+    end
+  end
 end
+
+
