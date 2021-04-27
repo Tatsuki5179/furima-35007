0deab73213f3a6481c3c3e94d7f240b1b83638bc
山下龍希 <tatsuki1105179@icloud.com>
2021-04-27 16:04:31 +0900

    商品出品機能修正3

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
     validates :product_name
     validates :product_description
     validates :product_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    validates :product_condition_id,  numericality: { other_than: 0, message: "Select" }
-    validates :shipping_charge_id, numericality: { other_than: 0, message: "Select" }
-    validates :shipping_area_id,  numericality: { other_than: 0, message: "Select" }
-    validates :days_ship_id,  numericality: { other_than: 0, message: "Select" }
-    validates :category_id,  numericality: { other_than: 0, message: "Select" }
+    validates :product_condition_id,  numericality: { other_than: 1, message: "Select" }
+    validates :shipping_charge_id, numericality: { other_than: 1, message: "Select" }
+    validates :shipping_area_id,  numericality: { other_than: 1, message: "Select" }
+    validates :days_ship_id,  numericality: { other_than: 1, message: "Select" }
+    validates :category_id,  numericality: { other_than: 1, message: "Select" }
     validates :image
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,18 @@
 FactoryBot.define do
 
-
   factory :item do
     product_name             {"hoge"}
     product_description      {Faker::Lorem.sentence}
-    product_condition_id     {1}
-    shipping_charge_id       {1}
-    shipping_area_id         {1}
-    days_ship_id             {1}
-    product_price            {"9999"}
-    user_id                  {1}
-  end
+    product_condition_id     {2}
+    shipping_charge_id       {2}
+    shipping_area_id         {2}
+    days_ship_id             {2}
+    category_id              {2}
+    product_price            { 9999 }
+
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end  
 end
